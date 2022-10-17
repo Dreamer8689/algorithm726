8481550afb32abcbc31f3b3b2f414d1773107695
dangzhengjun <582619192@qq.com>
2022-10-17 22:36:33 +0800

    dreamer:algorithm: 20221017  删除排序链表中的重复元素 leetcode83

---
+++ src/main/java/com/dreamer/algorithm/day20221017/LeetCode83.java
@@ -0,0 +1,43 @@
+package com.dreamer.algorithm.day20221017;
+
+/**
+ * leetcode-83
+ * 题目：给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表
+ * 示例：
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * <p>
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ */
+public class LeetCode83 {
+
+    // 遍历
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode cur = head;
+        while (cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+
+    // 递归
+    public ListNode deleteDuplicates1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        head.next = deleteDuplicates1(head.next);
+        if (head.val == head.next.val) head = head.next;
+        return head;
+    }
+
+}
